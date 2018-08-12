daed83ff54a04e7622855751e5d9ef7d8664297a
rendnil <andrew.lindner.15@gmail.com>
2018-08-12 15:29:10 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,54 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all = [ ]
+
+  def self.all
+    @@all
+  end
+
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
+  end
+
+  #all the paintings by a specific artist
+  def all_paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  #all the galleries that a specific artist has paintings in
+  def all_galleries
+    self.all_paintings.map do |painting|
+      painting.gallery
+    end.uniq
+  end
+
+  #all the cities that contain galleries that a specific artist has paintings in
+  def all_cities
+    self.all_galleries.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+  #average years of experience of all artists
+  def self.average_years
+    total_years = 0
+
+    Artist.all.each do |artist|
+      total_years += artist.years_active
+    end
+
+    average = total_years.to_f/(Artist.all.size)
+
   end
 
 
-end
+
+
+end #artist class end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,58 @@ class Gallery
 
   attr_reader :name, :city
 
-def initialize(name, city)
-  @name = name
-  @city = city
-end
+  @@all = [ ]
+
+  def self.all
+    @@all
+  end
 
 
-end
+  def initialize(name, city)
+    @name = name
+    @city = city
+    @@all << self
+  end
+
+  def self.all_cities
+    self.all.map do |gallery|
+      gallery.city
+    end.uniq
+  end
+
+ #get all paintings at a specific Gallery
+  def all_paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+
+  #all artists that have paintings at a specific Gallery
+  def all_artists
+    self.all_paintings.map do |painting|
+      painting.artist
+    end.uniq
+  end
+
+  #get a list of the names of artists that have paintings at a specific gallery
+  def artist_names
+    self.all_artists.map do |artist|
+      artist.name
+    end
+  end
+
+#Get the combined years of experience of all artists at a specific gallery
+  def total_years
+    total_years = 0
+    self.all_artists.each do |artist|
+      total_years += artist.years_active
+    end
+    total_years
+  end
+
+
+
+
+
+end #end gallery class
