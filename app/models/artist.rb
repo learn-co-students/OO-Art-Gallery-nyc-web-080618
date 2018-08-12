b545ace9959323d3f68925caa6809263f7e5d542
madtab7 <madtab7@gmail.com>
2018-08-12 15:11:43 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,45 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_years #iterates through all artists, totals years_active, divides by num of artists
+    total_artists = self.all.count
+    years = 0
+    self.all.each do |artist|
+      years += artist.years_active
+    end
+    (years/total_artists)
+  end
+
+  def paintings #iterates thru paintings, matches paintings to artist & returns
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries #iterates through #paintings and returns galleries
+    paintings.map do |painting|
+      painting.gallery.name
+    end
+  end
+
+  def cities #iterates through #paintings returns cities
+    paintings.map do |painting|
+      painting.gallery.city
+    end
   end
 
 
-end
+end #END CLASS
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,25 @@
 class Painting
+  attr_accessor :gallery
+  attr_reader :title, :style, :artist
 
-  attr_reader :title, :style
+  @@all = []
 
-  def initialize(title, style)
+  def initialize(artist, title, style, gallery)
+    @artist = artist
     @title = title
     @style = style
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.styles
+    self.all.map do |painting|
+      painting.style
+    end.uniq  
   end
 
 end
