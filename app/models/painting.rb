2fb907e24036a317a9a887b36d226eba578ab4a9
github username <github email address>
2018-08-12 20:00:54 -0400

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,27 @@
 class Painting
 
-  attr_reader :title, :style
+  attr_reader :title, :style, :artist, :gallery
 
-  def initialize(title, style)
+  @@all == []
+
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery = gallery
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.style
+    paint_style = Painting.all.map do |paint|
+      paint.style
+    end
+    paint_style.uniq
 end
+
+
+end ##end of class 
