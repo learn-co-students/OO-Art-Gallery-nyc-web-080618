e831ab97e0f683723beafc7da5da1a7ba6c09ec2
Jesse McReady <jessemcready@gmail.com>
2018-08-13 09:09:08 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,43 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
-  end
+    @@all << self
+  end # initialize
 
+  def self.all
+    @@all
+  end # self.all
 
-end
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end # Painting.all.select
+  end # paintings
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end.uniq # paintings.map
+  end # galleries
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end.uniq # galleries.map
+  end # cities
+
+  def self.average_years_experience
+    total_years = 0
+    @@all.each do |artist|
+      total_years += artist.years_active
+    end # all.each
+    total_years / @@all.length
+  end # self.average_years_experience
+
+end # class Artist
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,25 @@
 class Painting
 
-  attr_reader :title, :style
+  attr_reader :title, :style, :artist, :gallery
 
-  def initialize(title, style)
+  @@all = []
+
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
-  end
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end # initialize
 
-end
+  def self.all
+    @@all
+  end # self.all
+
+  def self.painting_styles
+    @@all.map do |painting|
+      painting.style
+    end.uniq! # @@all.map
+  end # self.painting_styles
+
+end # class Painting
