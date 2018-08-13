7a11023afeaebdc064950f2667487712b925a0e2
Johnny <johnnysoong@hotmail.com>
2018-08-12 20:34:16 -0400

    Art-Gallery completed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,44 @@
 class Artist
 
-  attr_reader :name
-
+  attr_reader :name, :years_active
+  @@all = []
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+#
+#
+  def galleries
+    Gallery.all.select do |gallery|
+     !(gallery.paintings & self.paintings).empty?
+    end
+  end
+#
+    def cities
+      array = []
+      self.galleries.each do |gallery|
+        array << gallery.city
+    end
+    array
+  end
+#
+def self.average_years
+  total = 0
+  Artist.all.each do |artist|
+    total += artist.years_active
+  end
+  (total / Artist.all.length.to_f).round
+end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,50 @@
 class Gallery
 
-  attr_reader :name, :city
-
-def initialize(name, city)
+  attr_reader :name, :city, :paintings
+  @@all = []
+def initialize(name, city, paintings)
   @name = name
   @city = city
+  @paintings = paintings
+  @@all << self
 end
 
+def self.all
+  @@all
+end
+
+def self.cities
+arr = []
+Gallery.all.each do |gallery|
+  arr << gallery.city
+end
+arr.uniq
+end
+
+def artists
+  array = []
+  self.paintings.each do |painting|
+    if Artist.all.include?(painting.artist)
+      array << painting.artist
+    end
+end
+array.uniq
+end
+
+def name_of_artists
+  array = []
+  artists.each do |artist|
+    array << artist.name
+  end
+  array.uniq
+end
+
+def total_experiences
+  total = 0
+  self.artists.each do |artist|
+  total += artist.years_active
+end
+total
+end
 
 end
