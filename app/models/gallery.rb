b3dc145ab976dea92d9b1b65dfd13b85c59c98d5
David <davidlago104@gmail.com>
2018-08-15 13:25:51 -0400

    done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,10 +2,44 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
 def initialize(name, city)
   @name = name
   @city = city
+  @@all << self
 end
 
+def self.all
+  @@all
+end
+
+def self.cities
+  self.all.map do |gallery|
+    gallery.city
+  end.uniq
+end
+
+def gallery_artists
+  gal = Painting.all.select do |painting|
+    painting.gallery == self
+  end
+  gal.map do |gallery|
+    gallery.artist
+  end
+end
+
+def gallery_names
+  gallery_artists.map do |artist|
+    artist.name
+  end
+end
+
+def gallery_exp
+  exp = gallery_artists.map do |artist|
+    artist.years_active
+  end
+  exp.reduce(:+) / exp.count
+end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,22 @@ def reload
   load 'config/environment.rb'
 end
 
+david = Artist.new("David", 6)
+jesse = Artist.new("Jesse", 7)
+mike = Artist.new("Mike", 10)
+wangtron = Artist.new("Wangtron", 20)
+luke = Artist.new("Luke", 13)
+
+g1 = Gallery.new("Vince", "Mohegan")
+g2 = Gallery.new("Granat", "Moscow")
+g3 = Gallery.new("Sheesh", "Gilbratar")
+g4 = Gallery.new("Gucci", "Turin")
+g5 = Gallery.new("Versace", "Venice")
+g6 = Gallery.new("YUHHH", "Venice")
+
+p1 = Painting.new("Sheesh wow", "Paint", david, g1)
+p2 = Painting.new("Gucci!!", "Sculpture", jesse, g2)
+p3 = Painting.new("Baka not nice", "Paint", mike, g3)
+p4 = Painting.new("GAINZ", "Sculpture", wangtron, g1)
+p5 = Painting.new("Woah", "Paint", david, g5)
 binding.pry
